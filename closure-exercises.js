90967435d329f8e18e4ab4064022f4e1768918b0
Gustavo Martinez <bhgustavo13@yahoo.com>
2017-10-17 11:05:51 -0500

    inital commit

---
+++ closure-exercises.js
@@ -0,0 +1,5 @@
+// 1) Make a counter function that returns a function that returns a successive number each time it is called.
+
+// 2) Allow the caller of counter to initialize the count to the first argument that's passed in
+
+// 3) Allow the counter to either increment 1 or decrement by 1 by calling the increment and decrement methods, respectively. Note: now the counter needs to return an object rather than a function.
